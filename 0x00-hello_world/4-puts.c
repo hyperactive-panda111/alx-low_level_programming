4d7371970e674c6afef35306b7537bb536bbec43
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-08 23:16:40 +0000

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ *
+ * 
+ * main - executing function
+ * return value : 0(Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multlingual puzzle");
+	return (0);
+}
