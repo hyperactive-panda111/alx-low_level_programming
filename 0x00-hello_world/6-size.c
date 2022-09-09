64db3415b1e9d7264615b6b1bcf9e982868692a0
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-09 00:17:51 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include<stdio>
+
+/**
+ *main - execting function
+ *
+ *Return: sucess = 0
+ */
+
+int main(void)
+{
+	char character;
+	int i;
+	float f;
+	long l;
+	long ll;
+
+	printf("size of a char: %lu byte\n", sizeof(character));
+	printf("size of an int: %lu byte(s)\n", size of(i));
+	printf("size of a float: %lu bytes\n", sizeof(f));
+	printf("size of a long: %lu bytes\n", sizeof(l));
+	printf("size of a long long: %lu bytes\n", sizeof(ll));
+
+	return (0);
+}
