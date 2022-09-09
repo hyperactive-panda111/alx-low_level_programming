9ee2bdf544d07000eee90e206667f83e3c9755cd
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-09 01:21:49 +0000

    final

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+#include <unistd.h>
+
+/*
+ * main - executing function
+ *
+ * Description: 'and that piece of art is useful"
+ *
+ *Return: always 0()sucess
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
