ed1b9770f448fb8533a97a3d7bf606c4620f7781
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-13 23:41:04 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print all lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i =97; i < 103; i++)
+		_putchar(i);
+	_putchar(10);
+}
