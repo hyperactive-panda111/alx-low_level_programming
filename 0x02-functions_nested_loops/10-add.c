6cabd109feb675b894d0ff19bec99680da007349
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-14 04:19:04 +0000

    added values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - addition of two integers
+ *
+ * @a: parameter
+ *
+ * @b: parameter
+ *
+ * Return: added value
+ */
+
+int add (int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
