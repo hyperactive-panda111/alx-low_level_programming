2d6e856bc71daa0231e8e7ed6d907b5840d45ac0
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-14 01:36:50 +0000

    adding 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value of input integer
+ *
+ * Return: absolute value of integer
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
