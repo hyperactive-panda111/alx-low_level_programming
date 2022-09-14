36e971f1dd60eb406f7c497d444ac05b69fe94ef
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-14 00:08:03 +0000

    adding 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,11 @@ void print_alphabet_x10(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		for (a = 97; a < 123;a++)
+	{
+		for (a = 97; a < 123; a++)
 		{
 			_putchar(a);
-			_putchar(10);
+	
 		}
+	_putchar(10);
 }
