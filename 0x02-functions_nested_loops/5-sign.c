67879af05918d1f468db68c57927e41e17746897
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-14 01:23:39 +0000

    adding 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,7 @@
 
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		_putchar(0);
-		return (0);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		_putchar(1);
 		return (1);
@@ -24,5 +19,10 @@ int print_sign(int n)
 	{
 		_putchar(-1);
 		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
 	}
 }
--- 0x02-functions_nested_loops/5-sign.c~
+++ 0x02-functions_nested_loops/5-sign.c~
@@ -20,7 +20,7 @@ int print_sign(int n)
 		_putchar(1);
 		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar(-1);
 		return (-1);
