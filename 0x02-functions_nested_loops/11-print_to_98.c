ea29778e714ae11b24da39a06bfb4b30593e3edc
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-14 04:27:22 +0000

    counting

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print sequentially from n to 98
+ *
+ * Return: 0(Success)
+ */
+
+void print_to_98
+{
+	int i, n;
+
+	for (i = n; i < 99; i++)
+	{
+		_putchar((n/10) + 48);
+		_putchar((n%10) + 48);
+		_putchar(44);
+		_putchar(32);
+	}
+}
