ba35f822e2caa54a9ceb70072720d1889cb34770
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-14 01:07:41 +0000

    adding 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign(int n) - prints the sign of the input integer
+ *
+ * @n: testing variable;
+ *
+ * Return: 1 if positive, 0 if zero or -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar(1);
+		return (1);
+	}
+	else
+	{
+		_putchar(-1);
+		return (-1);
+}
