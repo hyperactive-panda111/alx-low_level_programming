688e5f780ec4ffc757ed46498bdd4d6802f7576a
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-14 01:45:25 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of input integer
+ *
+ * @n: parameter for function
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
