607e06bc26bf977faf09e6e7083e9898e6a4cdf6
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-15 20:37:30 +0000

    change directory name

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if input is an uppercase character
+ * @c: input value
+ * Return: 1(Success), else 0
+ */
+
+int _isupper(int c)
+{
+	if (n >= 65 && n <= 90)
+		return (0);
+	else
+		return (1);
+}
