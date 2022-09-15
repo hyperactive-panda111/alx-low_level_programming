a1f275177f9b686323c673b2661e73081735a4e6
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-15 08:31:51 +0000

    pushing 0-main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#iclude "main.h"
+
+/**
+ * main - print negative, positive or zero based on input
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return 0;
+}
