c1b8b3552ee5157d185daa1fe59d4601a9dd5f05
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-15 23:38:28 +0000

    adding print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal of n length
+ * @n: parameter
+ * Return: void
+ */
+
+void print_diagonal(int n);
+{
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b =0; b < a; b++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		if (a < (n-1))
+			_putchar(10);
+	}
+}
