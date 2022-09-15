030ec3a7a299672d2b488c284bdb5d6251d6087f
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-15 23:58:19 +0000

    adding print_size.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h'
+
+/**
+* print_square - print n squares n times
+* @size: parameter
+* Return: void
+*/
+
+void print_square(int size)
+{
+	int a, b;
+
+	for (a=0; a < size; a++)
+	{
+		for (b=0; b < size; b++)
+		{
+			_putchar(35);
+		}
+		_putchar(10);
+	}
+	_putchar(10);
+}
