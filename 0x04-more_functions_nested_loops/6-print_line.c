329a78ad342a48c0364ecd8667fd915c5248d488
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-15 22:53:58 +0000

    add print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-		_putchar(10);
-	else
+	for (i = 0; i < n; i++)
 	{
-		for(i = 0; i < n; i++)
-		{
-			_putchar(95);
-		}
-		_putchar(10);
+		_putchar(95);
 	}
+	_putchar(10);
 }
