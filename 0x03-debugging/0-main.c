129a46df5dfa3b8e464cd2b54f0e6d9cea819111
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-15 08:43:10 +0000

    pushing 0-main.c and main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - print negative, positive or zero based on input
- *
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
