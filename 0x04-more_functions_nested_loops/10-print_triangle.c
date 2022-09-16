f8d69660893f5c7bf66630553802bd13100fcd02
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-16 02:30:15 +0000

    adding print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle
+ * @size: parameter
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	for(a = 1; a <= size; a++)
+	{
+		for (b = size; b > i; b--)
+		{
+			_putchar(32);
+		}
+			for (c = 1; c <= b; c++)
+			{
+				_putchar(35)
+			}
+		_putchar(10);
+		if (a != size)
+			_putchar(10);
+	}
+}
