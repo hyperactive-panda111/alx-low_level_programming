3daaa32f911fa7e827533b97fad9ad9bf32a7c84
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-19 21:35:46 +0000

    adding 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,11 +15,11 @@ void puts_half(char *str)
 			break;
 		length++;
 	}
-		
+
 	if (length % 2 == 1)
 		i = length / 2;
 	else
-		i = (length -1) / 2;
+		i = (length - 1) / 2;
 
 	for (i++; i < length; i++)
 		_putchar(str[i]);
