79025ff23578e2b5e8ba6bbcf5fdc45322d1e546
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-19 14:08:25 +0000

    adding 4-print_rev.c function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print charactersin reverse order
+ * @s: array of chars
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	
+	for (i = _strlen(s); i >= 0; i--)
+		_putchar(s[i]);
+	_putchar(10);
+}
+
