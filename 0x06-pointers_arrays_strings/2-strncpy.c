bbebd17d93bcb27ba4ed18a120ee5039f0f21f34
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-21 21:30:00 +0000

    adding 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strncpy - copy string using n bytes/characters
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		if (src[i] != '\0')
+			break;
+		dest[i] = src[0];
+	}
+	return (dest);
+}
+
