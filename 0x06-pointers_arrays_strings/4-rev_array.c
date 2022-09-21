4928e9a28d09bd9211340c813ec5afb428d3602c
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-21 23:29:38 +0000

    adding 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,9 +13,9 @@ void reverse_array(int *a, int n)
 
 	for (int i = 0; i < n / 2; i++)
 	{
-		temp = arr[i];
-		arr[i] = arr[n - i - 1];
-		arr[n - i - 1] = temp;
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
 }
 
