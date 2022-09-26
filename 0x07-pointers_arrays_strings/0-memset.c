45977de8d3261fb823e47e2fdada5726410378ee
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-26 15:17:49 +0000

    adding 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,14 +4,17 @@
  * _memset - fills the first n bytes of pointed by s with b
  * @b: characters
  * @n: number of bytes
+ * @s: memory area
  * Return: pointer to s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
+
+	return (s);
 }
 
