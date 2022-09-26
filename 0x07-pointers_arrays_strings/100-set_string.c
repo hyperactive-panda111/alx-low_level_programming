dc45effba2de9d4f1226eebbede64a7040d43796
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-26 23:24:25 +0000

    adding 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string -  change value of pointer
+ * @s: pointer to pointer
+ * @to: string
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
