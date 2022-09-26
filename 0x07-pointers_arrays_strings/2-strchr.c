6632cb82e352d43a3a1c881b907db8a79ef3fcdd
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-26 16:05:55 +0000

    adding 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - returns first occurrence of character
+ * @c: character
+ * Return: pointer to first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			break;
+	}
+
+	return (s + i);
+}
+
