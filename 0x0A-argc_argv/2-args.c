80c305b91c29162b22322db6b7e7050627161571
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-30 14:13:27 +0000

    adding 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - executing function
+ * argc: count
+ * argv: vector
+ * Return: 0 if success, else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		printf("%s", argv[i+1]);
+	}
+
+	return (0);
+}
+
