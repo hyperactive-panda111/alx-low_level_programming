0ffdbcc25368dbde6682bcf5e0f93574abc268af
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-30 14:22:13 +0000

    adding 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
-		for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i + 1]);
+		for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
