7e4974bc3af5ddea2f0f42647606bee5be1aae96
Yaw Amo Mensah <amomensah300@gmail.com>
2022-09-30 13:59:26 +0000

    adding 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include "main.h"
 /**
- * main - execution path
+ * main - prints out all of the arguments passed to it.
  * @argc: argument count
  * @argv: argument vector
- * Return: 1 if success, else 0
+ * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
-
-		printf("%d\n", argc - 1);
-
+	printf("%d\n", argc - 1);
+	argv[0] = 0;
 	return (0);
 }
 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,5 @@
 #define MAIN_H
 
 int _putchar(char s);
-
 #endif
 
