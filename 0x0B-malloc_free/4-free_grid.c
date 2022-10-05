50fb66c0733ff4a86d3fb15c4153a21c004533f5
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-05 00:42:05 +0000

    adding 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - releases dynamically allocated array
+ * @grid: array
+ * @height: dimension
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+	free(grid);
+}
+
