ddac64f4cc4049fb4f3d82b5fa2c840c5ffddfc1
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-06 20:49:51 +0000

    adding 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes for memory allocation
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *c;
+
+	c = malloc(sizeof(unsigned int) * b);
+	if (c == NULL)
+		exit(98);
+
+	return (c);
+}
+
