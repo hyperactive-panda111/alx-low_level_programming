ad3d139c3bb621ce63f71c0e84ccf4692a686f54
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-06 23:17:45 +0000

    adding 2-calloc.c(CORRECTED)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *c;
+	char *c;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (c == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (size * nmemb); i++)
 		c[i] = 0;
 
 	return (c);
