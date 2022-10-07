33615b71e115e267158cd27c3d083724a28e1061
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-07 00:53:58 +0000

    adding 3-array_range.c(EDITED)

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i <= max; i++, min++)
+	for (i = 0; min <= max; i++, min++)
 		array[i] = min;
 
 	return (array);
