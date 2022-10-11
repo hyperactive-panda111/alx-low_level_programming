2bba4ad39934724c594eab1acfb0cce956aa688d
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-11 02:36:29 +0000

    adding 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,17 +10,44 @@
  * Return: pointer to new struct
  */
 
-dog_t *new_dog(char *name, float dog, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
+	int i, iname, iowner;
 
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	p = malloc(sizeof(*p));
+	if (p == NULL || !(name) || !(owner))
+	{
+		free(p);
 		return (NULL);
+	}
 
-	p->name = name;
+	for (iname = 0; name[iname]; iname++)
+		;
+
+	for (iowner = 0; owner[iowner]; iowner++)
+		;
+
+	p->name = malloc(iname + 1);
+	p->owner = malloc(iowner + 1);
+
+	if (!(p->name) || !(p->owner))
+	{
+		free(p->name);
+		free(p->owner);
+		free(p);
+		return (NULL);
+	}
+
+	for (i = 0; i < iname; i++)
+		p->name[i] = name[i];
+	p->name[i] = '\0';
+
+	for (i = 0; i < iowner; i++)
+		p->owner[i] = owner[i];
+	p->owner[i] = '\0';
+
 	p->age = age;
-	p->owner = owner;
 
 	return (p);
 }
