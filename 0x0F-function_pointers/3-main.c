c8a0f4ed03934ced94863bfafac2355d8c5821dd
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-12 23:32:18 +0000

    adding assignment

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,7 +6,7 @@
  * @argc: count of arguments
  * @argv: pointer to array of pointers
  * Return: 0 on success, 98 on indaquate arguments, 99 if no operator
- */           
+ */
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-						
+
 	if (get_op_func(argv[2]) == NULL)
 		printf("Error\n"), exit(99);
 	if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
