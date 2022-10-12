11719d187c21e62d34e2ff3ad8794ba6250bcc1a
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-12 23:27:12 +0000

    adding assignment

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - determines appropriate operator based on input
+ * @s: operator
+ * Return: result of the operation
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+		    };
+
+	i = 0;
+	while (i < 5)
+	{
+		if (*ops[i].op == *s && !(*(s + 1)))
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
+
