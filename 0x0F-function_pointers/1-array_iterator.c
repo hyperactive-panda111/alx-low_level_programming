bde8577c79668c3a8bfae5395405157b5d7a3217
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-12 20:01:58 +0000

    adding 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints elements of an array
+ * @size: size of the array
+ * @array: pointer to the array
+ * @action: pointer to function
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array + i);
+	}
+	}
+}
+
