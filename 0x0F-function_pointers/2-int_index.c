3f6071d0821a168c4b1f0d10a20840b1716a66c4
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-12 21:03:10 +0000

    adding 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - runs a search for an integer
+ * @array: pointer to array
+ * @size: number of elements
+ * @cmp: pointer to function
+ * Return: index of matching condition, if successful else
+ * -1 for no match or wrong array size
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp || size <= 0)
+	{
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]));
+	return (i);
+	}
+
+	return (-1);
+}
+
