aa5a9fb943146d126a31e1c4f91d682be542c345
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-12 20:20:02 +0000

    adding 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * array_iterator - prints elements of an array
@@ -10,7 +11,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array && action)
 	{
