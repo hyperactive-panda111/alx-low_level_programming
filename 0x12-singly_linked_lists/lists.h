2afd915de1f6123314f9fbc9abf302bbc6826efe
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-20 22:42:59 +0000

    adding 0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,23 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
