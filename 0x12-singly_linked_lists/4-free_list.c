d59c1b158815da9565de40c587867e23238e2d1e
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-21 02:12:33 +0000

    adding 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list dynamically
+ * @head: singly linked list
+ * Return: freed memory
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while ((node  head) != NULL)
+	{
+		head = head->next;
+		free(node->str);
+		free(node);
+	}
+}
+
