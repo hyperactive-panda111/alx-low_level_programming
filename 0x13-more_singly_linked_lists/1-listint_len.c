8f4864ff8b7c23c68798cb67d2d9eb2b614de964
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-25 19:19:04 +0000

    adding 0-print_listint.c(EDIT)

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * lsitint_len - returns the number of elements in linked list
+ * @h: linked list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
+
