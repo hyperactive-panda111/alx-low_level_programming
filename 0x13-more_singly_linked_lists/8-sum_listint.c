814b37b3f4fce516b085edaa97f9db7ba2ad392e
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-26 02:54:34 +0000

    adding 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint -  prints sum of values in list
+ * @head: head of list
+ * Return: sum of values in list
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
+
