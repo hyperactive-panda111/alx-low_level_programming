a0db7b38f1db4d81ebdb77ed7bfc9c55ef0f794b
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-27 23:31:05 +0000

    adding refatored 0-binary-to-uint.c(EDIT1)

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (len--,b2 = 1; len >= 0; len--,b2 *= 2)
+	for (len--, b2 = 1; len >= 0; len--, b2 *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
