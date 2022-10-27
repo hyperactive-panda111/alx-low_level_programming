c5b6dd6d2b8cf5aebe594c4e8f46097a859e4a39
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-27 23:52:02 +0000

    adding 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * get_bit - get value of int at given index
+ * @n: input
+ * @index: index to search
+ * Return: value of bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
+}
+
