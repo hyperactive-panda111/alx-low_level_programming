031e71d380836f7c48b5ad85768374d18e255fb2
Yaw Amo Mensah <amomensah300@gmail.com>
2022-10-28 00:13:14 +0000

    adding 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * clear_bit - set value of bit to 0
+ * @n: input
+ * @index: index at which bit should change
+ * Return: changed bits of input
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n & ~(1 << index);
+	return ((index > 8 * sizeof(*n)) ? -1 : 1);
+}
+
