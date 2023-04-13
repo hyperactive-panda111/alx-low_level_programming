26f19948a52069aad75a471c4d7974d1b1a1fe7c
Amo Mensah <amomensah300@gmail.com>
2023-04-13 23:06:36 +0000

    making initial corrections

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,1 +1,10 @@
-#
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>  // printf function
+#include <stddef.h>  // size_t type
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
