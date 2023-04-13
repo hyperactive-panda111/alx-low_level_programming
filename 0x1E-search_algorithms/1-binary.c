a2a486c2976a7e9ae908ac1c0a086624714de759
Amo Mensah <amomensah300@gmail.com>
2023-04-13 23:39:37 +0000

    ading 1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search for value in a sorted array
+ * using the binary search algorithm
+ *
+ * @array: input array
+ * @size: size of input array
+ * @value: searched value
+ * Return: int value:search value or -1 if unsuccessful
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start = 0, end = size - 1;
+	size_t mid;
+
+	if (!array)
+		return (-1);
+
+	while (end >= start)
+	{
+		mid = (start + end) / 2;
+
+		if (value == array[mid])
+			return (mid);
+		else if (value > array[mid])
+			start += mid;
+		else
+			end -= mid;
+	}
+
+	return (-1);
+}
