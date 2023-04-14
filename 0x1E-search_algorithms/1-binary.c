8d220625b7a2a95e651ea17f14ae4db6fc7e5430
Amo Mensah <amomensah300@gmail.com>
2023-04-14 01:30:29 +0000

    adding corrections (v1 - 1-binary.c)

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,13 +13,24 @@
 int binary_search(int *array, size_t size, int value)
 {
 	size_t start = 0, end = size - 1;
-	size_t mid;
+	size_t mid, i;
 
 	if (!array)
 		return (-1);
 
+	
+
 	while (end >= start)
 	{
+		printf("Searching in array: ");
+		for (i = start; i <= end; i++)
+		{
+			if (i == end)
+				print("%d", array[i]);
+			else
+				print("%d ," array[i]);
+		}
+
 		mid = (start + end) / 2;
 
 		if (value == array[mid])
